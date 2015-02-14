80bf93b7a0c21fc884ef0c961ba96a6ae6194fc2
Chong Lian <alexchonglian@gmail.com>
2015-02-14 01:25:47 -0500

    3.33

--- SICP3.rkt
+++ SICP3.rkt
@@ -1632,7 +1632,7 @@ z2;((wow b) a b)
 |#
 
 
-'(exercise 3 27)
+'(exercise 3 27)#|
 (define (lookup key table)
   (let ((record (asso key (cdr table)))) ;; ignore the *table* label
     (if record
@@ -1669,6 +1669,458 @@ z2;((wow b) a b)
 
 (set! fib (memoize fib))
 ;(fib 100)
+|#
+
+
+
+;; 3.3.4 simulator for digital circuits
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; code in this section are reorganized to make it work
+; so the order is different from that in the book
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;; wires
+(define (make-wire)
+  (let ((signal-value 0)
+        (action-procedures '()))
+    
+    (define (set-my-signal! new-value)
+      (if (not (= signal-value new-value))
+          (begin (set! signal-value new-value)
+                 (call-each action-procedures))
+          'done))
+    
+    (define (accept-action-procedure! proc)
+      (set! action-procedures
+            (cons proc action-procedures))
+      (proc))
+    
+    (define (dispatch m)
+      (cond ((eq? m 'get-signal) signal-value)
+            ((eq? m 'set-signal!) set-my-signal!)
+            ((eq? m 'add-action!) accept-action-procedure!)
+            (else (display "unknown operation - wires"))))
+    dispatch))
+
+
+(define (call-each procedures)
+  (if (null? procedures)
+      'done
+      (begin 
+        ((car procedures))
+        (call-each (cdr procedures)))))
+
+(define (get-signal wire)
+  (wire 'get-signal))
+(define (set-signal! wire new-value)
+  ((wire 'set-signal!) new-value))
+(define (add-action! wire action-procedure)
+  ((wire 'add-action!) action-procedure))
+
+
+;; logical relationship
+(define (logical-not s)(cond ((= s 0) 1)((= s 1) 0)(else (display "error"))))
+(define (logical-and s1 s2) (and s1 s2))
+(define (logical-or s1 s2) (or s1 s2))
+
+;; agenda - i think of it as (clock with seq of funcs)
+;(make-agenda)
+;(empty-agenda? <agenda>)
+;(first-agenda-item <agenda>)
+;(remove-first-agenda-item! <agenda>)
+;(add-to-agenda! <time> <action> <agenda>)
+;(current-time <agenda>)
+
+(define (make-time-segment time queue)
+  (cons time queue))
+
+(define (segment-time s) (car s))
+
+(define (segment-queue s) (cdr s))
+
+(define (make-agenda) (list 0))
+
+(define (current-time agenda) (car agenda))
+
+(define (set-current-time! agenda time)
+  (set-car! agenda time))
+
+(define (segments agenda)(cdr agenda))
+
+(define (set-segments! agenda segments) (set-cdr! agenda segments))
+
+(define (first-segment agenda) (car (segments agenda)))
+
+(define (rest-segments agenda) (cdr (segments agenda)))
+
+(define (empty-agenda? agenda)
+  (null? (segments agenda)))
+
+
+(define (add-to-agenda! time action agenda)
+  (define (belongs-before? segments)
+    (or (null? segments)
+        (< time (segment-time (car segments)))))
+  (define (make-new-time-segment time action)
+    (let ((q (make-queue)))
+      (insert-queue! q action)
+      (make-time-segment time q)))
+  (define (add-to-segments! segments)
+    (if (= (segment-time (car segments)) time)
+        (insert-queue! (segment-queue (car segments))
+                       action)
+        (let ((rest (cdr segments)))
+          (if (belongs-before? rest)
+              (set-cdr! segments (cons (make-new-time-segment time action)
+                                       (cdr segments)))
+              (add-to-segments! rest)))))
+  (let ((segments (segments agenda)))
+    (if (belongs-before? segments)
+        (set-segments! agenda (cons (make-new-time-segment time action) 
+                                    segments))
+        (add-to-segments! segments))))
+
+(define (remove-first-agenda-item! agenda)
+  (let ((q (segment-queue (first-segment agenda))))
+    (delete-queue! q)
+    (if (empty-queue? q)
+        (set-segments! agenda (rest-segments agenda)))))
+
+(define (first-agenda-item agenda)
+  (if (empty-agenda? agenda)
+      (display "Agenda empty - first-agenda-item")
+      (let ((first-seg (first-segment agenda)))
+        (set-current-time! agenda (segment-time first-seg))
+        (front-queue (segment-queue first-seg)))))
+
+; behavior; add action to agenda
+(define (after-delay delay action)
+  (add-to-agenda! (+ delay (current-time the-agenda))
+                  action
+                  the-agenda))
+
+
+;; run funcs in queue of agenda till its empty
+(define (propagate)
+  (if (empty-agenda? the-agenda)
+      'done
+      (let ((first-item (first-agenda-item the-agenda)))
+        (first-item)
+        (remove-first-agenda-item! the-agenda)
+        (propagate))))
+
+;; 
+(define (probe name wire)
+  (add-action! wire
+               (lambda ()
+                 (newline)
+                 (display name)
+                 (display " ")
+                 (display (current-time the-agenda))
+                 (display "  New-value = ")
+                 (display (get-signal wire)))))
+
+(define the-agenda (make-agenda))
+(define inverter-delay 2)
+(define and-gate-delay 3)
+(define or-gate-delay 5)
+
+(define input-1 (make-wire))
+(define input-2 (make-wire))
+(define sum (make-wire))
+(define carry (make-wire))
+
+
+
+
+; a behavior that connect wires
+(define (inverter input output)
+  (define (invert-input)
+    (let ((new-value (logical-not (get-signal input))))
+      (after-delay inverter-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! input invert-input)
+  'ok)
+
+; a behavior that connect wires
+(define (and-gate a1 a2 output)
+  (define (and-action-procedure)
+    (let ((new-value (logical-and (get-signal a1) (get-signal a2))))
+      (after-delay and-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 and-action-procedure)
+  (add-action! a2 and-action-procedure)
+  'ok)
+
+; a behavior that connect wires
+(define (or-gate a1 a2 output)
+  (define (or-action-procedure)
+    (let ((new-value (logical-or (get-signal a1) (get-signal a2))))
+      (after-delay or-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 or-action-procedure)
+  (add-action! a2 or-action-procedure)
+  'ok)
+
+; a behavior that connect wires
+(define (half-adder a b s c)
+  (let ((d (make-wire))
+        (e (make-wire)))
+    (or-gate a b d)
+    (and-gate a b c)
+    (inverter c e)
+    (and-gate d e s)
+    'ok))
+
+; a behavior that connect wires
+(define (full-adder a b c-in sum c-out)
+  (let ((s (make-wire))
+        (c1 (make-wire))
+        (c2 (make-wire)))
+    (half-adder b c-in s c1)
+    (half-adder a s sum c2)
+    (or-gate c1 c2 c-out)
+    'ok))
+
+;(probe 'sum sum)
+; sum 0 New-value = 0
+
+;(probe 'carry carry)
+; carry 0 New-value = 0
+
+;(half-adder input-1 input-2 sum carry)
+;ok
+
+;(set-signal! input-1 1)
+;done
+
+;(propagate)
+;sum 8 New-value = 1
+;done
+
+;(set-signal! input-2 1)
+;done
+
+;(propagate)
+;carry 11 New-value = 1
+;sum 16 New-value = 0
+;done
+
+
+(define a (make-wire))
+(define b (make-wire))
+(define c (make-wire))
+(define d (make-wire))
+(define e (make-wire))
+(define s (make-wire))
+
+;(or-gate a b d)
+;(and-gate a b c)
+;(inverter c e)
+;(and-gate d e s)
+
+
+
+;(get-signal <wire>)
+
+;(set-signal! <wire> <new value>)
+
+;(add-action! <wire> <proc>)
+
+;(after-delay <delay> <proc>)
+
+(define (inverter input output)
+  (define (invert-input)
+    (let ((new-value (logical-not (get-signal input))))
+      (after-delay inverter-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! input invert-input)
+  'ok)
+
+
+(define (logical-not s)
+  (cond
+    ((= s 0) 1)
+    ((= s 1) 0)
+    (else (display "error"))))
+
+
+(define (and-gate a1 a2 output)
+  (define (and-action-procedure)
+    (let ((new-value (logical-and (get-signal a1) (get-signal a2))))
+      (after-delay and-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 and-action-procedure)
+  (add-action! a2 and-action-procedure)
+  'ok)
+
+
+
+'(exercise 3 28)
+(define (or-gate a1 a2 output)
+  (define (or-action-procedure)
+    (let ((new-value (logical-or (get-signal a1) (get-signal a2))))
+      (after-delay or-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 or-action-procedure)
+  (add-action! a2 or-action-procedure)
+  'ok)
+
+
+'(exercise 3 29)
+;DeMorgan Law? a|b = !( (!a) & (!b) ) look up what it is later
+;
+;             +---+
+; a -->INV--> |   |
+;             |AND|-->INV--> output
+; b -->INV--> |   |
+;             +---+
+;
+; time delay is 2 inv + 1 and
+;
+#|
+(define (or-gate a1 a2 output)
+  (let ((w1 (wire))
+        (w2 (wire))
+        (o (wire)))
+    (inverter a1 w1)
+    (inverter a2 w2)
+    (and-gate w1 w2 o)
+    (inverter o output)))
+|#
+
+'(exercise 3 30)
+(define (ripple-carry-adder a b s c)
+  (let ((c-in (make-wire)))
+    (if (null? (cdr a))
+        (set-signal! c-in 0)
+        (ripple-carry-adder (cdr a) (cdr b) (cdr s) c-in))
+    (full-adder (car a) (car b) c-in (car s) c)))
+
+;; delay proportional to n
+
+
+'(exercise 3 31)#|
+;Explain why accept-action-procedure is written like this.
+(define (accept-action-procedure! proc)
+  (set! action-procedures
+        (cons proc action-procedures))
+  (proc))
+
+;instead of 
+(define (accept-action-procedure! proc)
+  (set! action-procedures
+        (cons proc action-procedures)))
+
+bcs, i have to trigger it to let it cascade. 
+or it will be in equilibrium (all zeros)
+
+|#
+
+
+'(exercise 3 32)
+; obviously temperal order matters
+
+
+;; 3.3.5 propagation of constraints
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; code in this section are reorganized to make it work
+; so the order is different from that in the book
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;(has-value? <connector>)
+;(get-value  <connector>)
+;(set-value! <connector> <new-value> <informant>)
+;(forget-value! <connector> <retractor>)
+;(connect <connector> <new-constraint>)
+
+(define (adder a1 a2 sum)
+  (define (process-new-value)
+    (cond ((and (has-value? a1) (has-value? a2))
+           (set-value! sum
+                       (+ (get-value a1) (get-value a2))
+                       me))
+          ((and (has-value? a1) (has-value? sum))
+           (set-value! a2
+                       (- (get-value sum) (get-value a1))
+                       me))
+          ((and (has-value? a2) (has-value? sum))
+           (set-value! a1
+                       (- (get-value sum) (get-value a2))
+                       me))))
+  (define (process-forgot-value)
+    (forgot-value! sum me)
+    (forgot-value! a1 me)
+    (forgot-value! a2 me)
+    (process-new-value))
+  (define (me request)
+    (cond ((eq? request 'I-have-a-value) (process-new-value))
+          ((eq? request 'I-lost-my-value) (process-forgot-value))
+          (else (display "unknown"))))
+  (connect a1 me)
+  (connect a2 me)
+  (connect sum me)
+  me)
+
+
+
+(define C (make-connector))
+(define F (make-connector))
+(celsius-fahrenheit-converter C F)
+
+(define (celsius-fahrenheit-converter c f)
+  (let ((u (make-connector))
+        (v (make-connector))
+        (x (make-connector))
+        (y (make-connector))
+        (z (make-connector)))
+    (multiplier c w u)
+    (multiplier v x u)
+    (adder v y f)
+    (constant 9 w)
+    (constant 5 x)
+    (constant 32 y)
+    (display "ok")))
+
+
+(proble "Celsius temp" C)
+(proble "Fahrenheit temp" F)
+
+(set-value! C 25 'user)
+;Probe: Celsius temp = 25
+;Probe: Fahrenheit temp = 77
+;done
+
+(set-value! F 212 'user)
+;Error! Contradiction (77 212)
+
+(forget-value! C 'user)
+;Probe: Celcius temp = ?
+;Probe: Fahrenheit temp = ?
+;done
+
+(set-value! F 212 'user)
+;Probe: Celcius temp = 212
+;Probe: Fahrenheit temp = 100
+;done
+
+
+
+
+
+
+
+
 
 
 
